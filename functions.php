3f2720b47e4d690996393a8a164ae39421e3c1cc
Joe Mizzi <themizzi@me.com>
2015-03-16 14:43:13 -0500

    Reordered and named sidebar-1

--- functions.php
+++ functions.php
@@ -17,14 +17,26 @@ function theme_enqueue_styles() {
  * @link https://codex.wordpress.org/Function_Reference/register_sidebar
  */
 function themizzerables_widgets_init() {
-        register_sidebar( array(
-                'name'          => __( 'Below Sidebar Menu', 'twentyfifteen' ),
-                'id'            => 'sidebar-2',
-                'description'   => __( 'Add widgets here to appear in your sidebar.', 'twentyfifteen' ),
-                'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-                'after_widget'  => '</aside>',
-                'before_title'  => '<h2 class="widget-title">',
-                'after_title'   => '</h2>',
-        ) );
+    unregister_sidebar( 'sidebar-1' );
+
+    register_sidebar( array(
+        'name'          => __( 'Above Sidebar Menu', 'twentyfifteen' ),
+        'id'            => 'sidebar-1',
+        'description'   => __( 'Add widgets here to appear in your sidebar above the menu.', 'twentyfifteen' ),
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>'
+    ) );
+
+    register_sidebar( array(
+        'name'          => __( 'Below Sidebar Menu', 'twentyfifteen' ),
+        'id'            => 'sidebar-2',
+        'description'   => __( 'Add widgets here to appear in your sidebar below the menu.', 'twentyfifteen' ),
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>'
+    ) );
 }
-add_action( 'widgets_init', 'themizzerables_widgets_init' );
+add_action( 'widgets_init', 'themizzerables_widgets_init', 11 );
